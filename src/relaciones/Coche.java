4dc33593d363e17bc34cdecc9d17499fc3c4cc63
FacilitadorDAM <145330715+ProfeFS@users.noreply.github.com>
2024-01-30 18:23:48 +0100

    Delete src/relaciones directory

--- src/relaciones/Coche.java
+++
@@ -1,6 +0,0 @@
-package relaciones;
-class Coche {
-    public void mover() {
-        System.out.println("El coche está en movimiento");
-    }
-}
--- src/relaciones/Habitacion.java
+++
@@ -1,4 +0,0 @@
-package relaciones;
-class Habitacion {
-    // detalles de la clase Habitacion
-}
