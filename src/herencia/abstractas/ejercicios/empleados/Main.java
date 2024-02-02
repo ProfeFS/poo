bff3f676a69937ba17e2e6f052da091668a7be39
ProfeFS <profefs@outlook.com>
2024-02-02 14:27:48 +0100

    Agregado empleado fijo en caso de uso

--- src/herencia/abstractas/ejercicios/empleados/Main.java
+++ src/herencia/abstractas/ejercicios/empleados/Main.java
@@ -5,16 +5,17 @@ public class Main {
         Empleado empleadoRemoto = new EmpleadoRemoto("Luis", "002", "Desarrollo", 160, 20.0, 150);
 
         empleadoOficina.mostrarInformacion();
-        System.out.println("Salario: $" + empleadoOficina.calcularSalario());
+        System.out.println("Salario: " + empleadoOficina.calcularSalario());
 
         System.out.println("---");
 
         empleadoRemoto.mostrarInformacion();
-        System.out.println("Salario: $" + empleadoRemoto.calcularSalario());
+        System.out.println("Salario: " + empleadoRemoto.calcularSalario());
         
         EmpleadoFijo empleadoFijo = new EmpleadoFijo("Elena", "004", "Marketing", 60000, 5000);
 
+        System.out.println("---");
         empleadoFijo.mostrarInformacion();
-        System.out.println("Salario Total: $" + empleadoFijo.calcularSalario());
+        System.out.println("Salario Total: " + empleadoFijo.calcularSalario());
     }
 }
