a69514f07c8938f3c48231639d1ab21e72a38432
ProfeFS <profefs@outlook.com>
2024-02-02 11:37:57 +0100

    Revert "Delete src/ejercicio_biblioteca directory"
    This reverts commit 04ebe7bc78a45f772f498d3a29b3e366959ff3ed.

---
+++ src/ejercicio_biblioteca/Prestamo.java
@@ -0,0 +1,66 @@
+package ejercicio_biblioteca;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+public class Prestamo {
+
+	int cod;
+	Usuario usuario;
+	ArrayList<Libro> libros;
+	LocalDate fechaPrestamo;
+	LocalDate fechaDevolucion;
+
+	public Prestamo(ArrayList<Libro> libros, Usuario usuario) {
+		this.libros = libros;
+		this.usuario = usuario;
+		this.fechaPrestamo = LocalDate.of(2024, 1, 15);
+		this.fechaDevolucion = null;
+		setStatusLibros(libros);
+
+	}
+
+	private void setStatusLibros(ArrayList<Libro> libros) {
+		for (Libro libro : libros) {
+			libro.setIsDisponible(false);
+		}
+
+	}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public ArrayList<Libro> getLibros() {
+		return libros;
+	}
+
+	public void setLibros(ArrayList<Libro> libros) {
+		this.libros = libros;
+	}
+
+	public LocalDate getFechaPrestamo() {
+		return fechaPrestamo;
+	}
+
+	public void setFechaPrestamo(LocalDate fechaPrestamo) {
+		this.fechaPrestamo = fechaPrestamo;
+	}
+
+	public LocalDate getFechaDevolucion() {
+		return fechaDevolucion;
+	}
+
+	public void setFechaDevolucion(LocalDate fechaDevolucion) {
+		this.fechaDevolucion = fechaDevolucion;
+	}
+
+	@Override
+	public String toString() {
+		return "Prestamo [cod=" + cod + ", usuario=" + usuario + ", libros=" + libros + ", fechaPrestamo="
+				+ fechaPrestamo + ", fechaDevolucion=" + fechaDevolucion + "]";
+	}
+	
+	
+
+}
